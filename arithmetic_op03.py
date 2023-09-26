493968b5e009003d3a95efdde8e05855c657d5ca
allamurodxakimov <xakimov2806@gmail.com>
2023-09-25 17:11:06 -0700

    aritmetic 3

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,7 +1,10 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+number  = 48
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+a1 = 48/10
+a2 = 48%10
+b1 = a2*10
+answer = b1 + a1
 #Print the value of the 'answer'
-
+print(int(answer))
 
